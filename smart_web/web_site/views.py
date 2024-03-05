7a7d9b5aaae4e53cddd78cb5fb001bee93d2b332
Hz300 <81933727+Hz300@users.noreply.github.com>
2024-03-05 11:08:14 -0600

    aded modals to properties

--- smart_web/web_site/views.py
+++ smart_web/web_site/views.py
@@ -142,9 +142,30 @@ def portfolio(request):
     return render(request, "web_site/portfolio.html", context)
 
 def portfolio_es(request):
-   active_props = SMRTPROPERTIES.objects.filter(imb_Active=True)
-   context = {"props": active_props}
-   return render(request, "web_site/portfolio_es.html",context)
+    # Fetch distinct values for each dropdown
+    cities = SMRTPROPERTIES.objects.filter(imb_Active=True).values_list('imb_City', flat=True).distinct()
+    property_types = SMRTPROPERTIES.objects.filter(imb_Active=True).values_list('imb_Type', flat=True).distinct()
+
+    # Filter properties based on user input
+    if request.method == 'GET':
+        location_filter = request.GET.get('location_filter')
+        property_type_filter = request.GET.get('property_type_filter')
+        # Add more filters for other dropdowns as necessary
+
+        active_props = SMRTPROPERTIES.objects.filter(imb_Active=True)
+        if location_filter:
+            active_props = active_props.filter(imb_City=location_filter)
+        if property_type_filter:
+            active_props = active_props.filter(imb_Type=property_type_filter)
+        # Add more filters for other dropdowns as necessary
+
+    context = {
+        "props": active_props,
+        "cities": cities,
+        "property_types": property_types,
+        # Add more for other dropdowns as necessary
+    }
+    return render(request, "web_site/portfolio_es.html", context)
 
 def contact_es(request):
     form = SMRTWEBFORMForm()  # Define form outside the conditional block
